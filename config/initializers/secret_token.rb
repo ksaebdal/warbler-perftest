cbdafa632930c37be4aa212f1cc6372fa158c247
Brian Riddle <brian.riddle@tv4.se>
2011-01-21 08:08:53 +0100

    fresh rails install + .rvmrc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WarblerPerftest::Application.config.secret_token = '6fe8b27eedb6a3d958ab4b0bbcc9553754594d8a338335ba4f90a12fadecd2f46d6ebe69468f20f498912ada88be235c805bbf245e51f970fbc282e8ba5cdeea'
